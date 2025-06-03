599addb8a5decfdd383635637e8c910c456a4408
Barmoo <armoobelinda92@gmail.com>
2025-06-03 19:15:11 +0000

    changes added and staged

--- classic-app/src/App.jsx
+++ classic-app/src/App.jsx
@@ -5,6 +5,7 @@ import Product from "./components/Product";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import About from "./Pages/About";
 
 
 
@@ -17,7 +18,9 @@ const App = () => {
             <Testimonials/>
             <Contact/>
             <Footer/>
+           
         </div>
+
         
     )
 
